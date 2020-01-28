2251d18af221206b2e2d447165d71a897bc1808d
Kengo <funeace@gmail.com>
2020-01-28 23:00:45 +0900

    [Finish]3-8

--- sample_app/app/models/list.rb
+++ sample_app/app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ApplicationRecord
+  # 実際のカラムはimage_idだが、refileの命名規則により、「_id」を除いたカラム名を指定する
+  attachment :image
 end
--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+#Refileでこけたので追記
+Refile.secret_key = 'bbe33315a62fb10e4f77f750a53e04f058af82dd4d20efa78acd80096dd563714ea3b7fcb9fc4cf41ad667466d971860fc7057b21afce288c335b876f1d10ac2'
